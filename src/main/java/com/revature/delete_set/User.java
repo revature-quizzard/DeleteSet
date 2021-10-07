6d7188d49c218fd8258f3eb558740b45808e1601
heraldOfMechanus <jccallahan11@gmail.com>
2021-10-06 22:46:38 -0500

    Added lombok constructors to the user POJO. It should now build.

--- src/main/java/com/revature/delete_set/User.java
+++ src/main/java/com/revature/delete_set/User.java
@@ -1,7 +1,9 @@
 package com.revature.delete_set;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @DynamoDbBean
 public class User {
 
